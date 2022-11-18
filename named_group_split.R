99d2e8b8eee3eb7a493c630f8e45fffb50838596
srk7774 <ramakrishna.salagrama@gmail.com>
2022-11-18 10:45:57 +0530

    Update named_group_split.R

--- named_group_split.R
+++ named_group_split.R
@@ -7,4 +7,9 @@ named_group_split <- function(.tbl, ...) {
     rlang::set_names(names)
 }
 
+quoted_col_names = function(x){
+  x_split = stringr::str_split(x, "\\s+")
+  as.character(x_split) |> cat()
+}
+
 devtools::source_url("https://raw.githubusercontent.com/srk7774/R4ds/main/named_group_split.R")
